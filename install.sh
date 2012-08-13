cbc784e46ec09c65a3640e7fb66127d2fe52e8db
yamagu <www.yamagu@gmail.com>
2012-08-13 12:28:34 +0900

    install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-DOT_FILES=(.vim .vimrc .screenrc .gitignore .gitconfig)
+DOT_FILES=(.vim .vimrc .screenrc .gitignore .gitconfig .vimrc.bundle .vimrc.plugin_setting)
 
 for file in ${DOT_FILES[@]}
 do
