afd21f9a733c0a65df223290384d8d5f2f2c2e74
yamagu <www.yamagu@gmail.com>
2012-08-13 12:33:47 +0900

    add .gitmodules to install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-DOT_FILES=(.vim .vimrc .screenrc .gitignore .gitconfig .vimrc.bundle .vimrc.plugin_setting)
+DOT_FILES=(.vim .vimrc .screenrc .gitignore .gitconfig .gitmodules .vimrc.bundle .vimrc.plugin_setting)
 
 for file in ${DOT_FILES[@]}
 do
