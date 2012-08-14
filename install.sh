2db2d3fb3441aad0741350cbb367c2b97650f77c
yamagu <www.yamagu@gmail.com>
2012-08-15 06:58:22 +0900

    PHP辞書ファイルの生成

--- install.sh
+++ install.sh
@@ -19,16 +19,23 @@ done
 cd ~/
 # phpマニュアルのダウンロード
 if [ ! -d ~/docs/php-chunked-xhtml ]; then
-	mkdir ~/docs && cd ~/docs
-	wget -O php_manual_ja.tar.gz http://jp.php.net/get/php_manual_ja.tar.gz/from/this/mirror
-	tar xzf php_manual_ja.tar.gz
-	rm php_manual_ja.tar.gz
+	mkdir -p ~/docs && cd ~/docs
+	# english
+	wget -O php_manual.tar.gz http://jp.php.net/get/php_manual_en.tar.gz/from/this/mirror
+	# japanese
+	# wget -O php_manual.tar.gz http://jp.php.net/get/php_manual_ja.tar.gz/from/this/mirror
+	tar xzf php_manual.tar.gz
+	rm php_manual.tar.gz
 fi
 
 # php辞書の作成
 PHP_DICT=~/.vim/dict/php.dict
-php -r '$f=get_defined_functions();echo join("\n", $f["internal"]);'|sort > PHP_DICT
-#php -r 'echo join("\n", get_declared_classes());'|sort >> PHP_DICT
-#php -r 'echo join("\n", get_declared_interfaces());'|sort >> PHP_DICT
-#php -r 'echo join("\n", get_defined_constants());'|sort >> PHP_DICT
+# 関数
+php -r '$f=get_defined_functions();echo join("\n", $f["internal"]);'|sort > $PHP_DICT
+# クラス
+php -r 'echo join("\n", get_declared_classes());'|sort >> $PHP_DICT
+# インターフェース
+php -r 'echo join("\n", get_declared_interfaces());'|sort >> $PHP_DICT
+# 定数
+php -r '$c=get_defined_constants();echo join("\n", array_keys($c));'|sort >> $PHP_DICT
 
